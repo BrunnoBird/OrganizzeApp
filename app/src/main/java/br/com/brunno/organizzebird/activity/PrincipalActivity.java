69cf6e1856764ab38f961d7abf04236a173c6778
brunnobird <bird.tec.gonzalez@gmail.com>
2022-02-24 17:31:08 -0300

    feat: calendar

--- app/src/main/java/br/com/brunno/organizzebird/activity/PrincipalActivity.java
+++ app/src/main/java/br/com/brunno/organizzebird/activity/PrincipalActivity.java
@@ -3,27 +3,51 @@ package br.com.brunno.organizzebird.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import br.com.brunno.organizzebird.R;
 
 public class PrincipalActivity extends AppCompatActivity {
 
-    private FloatingActionButton fab;
+    private MaterialCalendarView calendarView;
+    private TextView textoSaudacao, textoSaldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        textoSaldo = findViewById(R.id.textSaldo);
+        textoSaudacao = findViewById(R.id.textSaudacao);
+        calendarView = findViewById(R.id.calendarView);
+
+        configuraCalendarView();
+
 
 //        fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show());
 
+    }
+
+    private void configuraCalendarView() {
+        CharSequence[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 
     public void adicionarReceita(View view) {
@@ -33,4 +57,6 @@ public class PrincipalActivity extends AppCompatActivity {
     public void adicionarDespesa(View view) {
         startActivity(new Intent(this, DespesasActivity.class));
     }
+
+
 }
--- app/src/main/java/br/com/brunno/organizzebird/activity/ReceitasActivity.java
+++ app/src/main/java/br/com/brunno/organizzebird/activity/ReceitasActivity.java
@@ -1,16 +1,129 @@
 package br.com.brunno.organizzebird.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.brunno.organizzebird.R;
+import br.com.brunno.organizzebird.config.ConfiguracaoFirebase;
+import br.com.brunno.organizzebird.helper.Base64Custom;
+import br.com.brunno.organizzebird.helper.DateCustom;
+import br.com.brunno.organizzebird.model.Movimentacao;
+import br.com.brunno.organizzebird.model.Usuario;
 
 public class ReceitasActivity extends AppCompatActivity {
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private Double receitaTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receitas);
+
+        campoValor = findViewById(R.id.editValorReceita);
+        campoData = findViewById(R.id.editDataReceita);
+        campoDescricao = findViewById(R.id.editDescricaoReceita);
+        campoCategoria = findViewById(R.id.editCategoriaReceita);
+        FloatingActionButton fab = findViewById(R.id.fabSalvarReceita);
+
+        campoData.setText(DateCustom.dataAtual());
+
+        recuperaReceitaTotal();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salvarReceita(v);
+            }
+        });
     }
+
+    private Boolean validarCamposReceita(){
+        String textoValor = campoValor.getText().toString();
+        String textoData = campoData.getText().toString();
+        String textoCategoria = campoCategoria.getText().toString();
+        String textoDescricao = campoDescricao.getText().toString();
+
+        if (textoValor.length() > 0) {
+            if (!textoData.isEmpty()) {
+                if (!textoCategoria.isEmpty()) {
+                    if (!textoDescricao.isEmpty()) {
+                        return true;
+                    } else {
+                        Toast.makeText(ReceitasActivity.this, "Descricao não foi preenchida!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(ReceitasActivity.this, "Categoria não foi preenchida!", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(ReceitasActivity.this, "Data não foi preenchida!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(ReceitasActivity.this, "Valor não foi preenchido!", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    public void atualizarReceita(Double receita) {
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+
+        usuarioRef.child("receitaTotal").setValue(receita);
+    }
+
+    public void salvarReceita(View view) {
+        if( validarCamposReceita() ){
+            String data = campoData.getText().toString();
+            Double valorRecuperado = Double.parseDouble(campoValor.getText().toString());
+
+            Movimentacao movimentacao = new Movimentacao();
+            movimentacao.setValor( valorRecuperado );
+            movimentacao.setCategoria( campoCategoria.getText().toString() );
+            movimentacao.setDescricao( campoDescricao.getText().toString() );
+            movimentacao.setData( data );
+            movimentacao.setTipo( "r" );
+
+            Double receitaAtualizada = receitaTotal + valorRecuperado;
+            atualizarReceita( receitaAtualizada );
+
+            movimentacao.salvar(data);
+
+        }
+    }
+
+    public void recuperaReceitaTotal() {
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Usuario usuario = snapshot.getValue( Usuario.class );
+                receitaTotal = usuario.getReceitaTotal();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
